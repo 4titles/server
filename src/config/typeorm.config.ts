5c1fcf70bebc3d8ca040c9ad878bd78ac268f63c
aveok4now <aveok4now@gmail.com>
2024-12-02 21:39:46 +0300

    chore: update typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,27 +11,22 @@ export interface ITypeORMConfig {
     autoLoadEntities: boolean
     synchronize: boolean
     retryAttempts: number
-    // replication?: {
-    //     master: {
-    //         host: string
-    //         port: number
-    //         username: string
-    //         password: string
-    //         database: string
-    //     }
-    //     slaves: Array<{
-    //         host: string
-    //         port: number
-    //         username: string
-    //         password: string
-    //         database: string
-    //     }>
-    // }
-    host: string
-    port: number
-    username: string
-    password: string
-    database: string
+    replication?: {
+        master: {
+            host: string
+            port: number
+            username: string
+            password: string
+            database: string
+        }
+        slaves: Array<{
+            host: string
+            port: number
+            username: string
+            password: string
+            database: string
+        }>
+    }
     logging: boolean
 }
 
@@ -42,29 +37,24 @@ const config: ITypeORMConfig = {
     autoLoadEntities: true,
     synchronize: true, //@todo: change
     retryAttempts: 15,
-    // replication: {
-    //     master: {
-    //         host: process.env.DB_HOST || 'postgres',
-    //         port: parseInt(process.env.DB_PORT || '5432', 10),
-    //         username: process.env.DB_USERNAME,
-    //         password: process.env.DB_PASSWORD,
-    //         database: process.env.DB_DATABASE,
-    //     },
-    //     slaves: [
-    //         {
-    //             host: process.env.DB_SLAVE_HOST || 'postgres',
-    //             port: parseInt(process.env.DB_SLAVE_PORT || '5432', 10),
-    //             username: process.env.DB_SLAVE_USERNAME,
-    //             password: process.env.DB_SLAVE_PASSWORD,
-    //             database: process.env.DB_SLAVE_DATABASE,
-    //         },
-    //     ],
-    // },
-    host: process.env.DB_HOST || 'postgres',
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    replication: {
+        master: {
+            host: process.env.DB_HOST || 'postgres',
+            port: parseInt(process.env.DB_PORT || '5432', 10),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        },
+        slaves: [
+            {
+                host: process.env.DB_SLAVE_HOST || 'postgres',
+                port: parseInt(process.env.DB_SLAVE_PORT || '5432', 10),
+                username: process.env.DB_SLAVE_USERNAME,
+                password: process.env.DB_SLAVE_PASSWORD,
+                database: process.env.DB_SLAVE_DATABASE,
+            },
+        ],
+    },
     logging: false,
 }
 
